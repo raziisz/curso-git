f64eaa2fa170d2ccdeb13a4b22635a290ba1711e
Luiz Felipe <raziel_libertino@hotmail.com>
2019-01-28 23:08:32 -0400

    Foi acrescentado a msg de despedida

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -10,6 +10,7 @@ namespace ProjetoTeste
             
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia");
+            Console.WriteLine("Fui partiu, aonde? É o Mandela");
             Console.ReadLine();
         }
     }
