a68fe8a82278bd59e5dd98a00ce4cda4f320ab68
raziisz <raziel_libertino@hotmail.com>
2019-01-28 22:01:42 -0400

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ ProjetoTeste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjetoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo");
+
+            Console.ReadLine();
+        }
+    }
+}
