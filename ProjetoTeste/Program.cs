297cb1598954ffbcc015e500eea402aa162fa82f
raziisz <raziel_libertino@hotmail.com>
2019-01-28 22:05:50 -0400

    Acrescentada uma mensagem de  bom dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -7,6 +7,7 @@ namespace ProjetoTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá Mundo");
+            Console.WriteLine("Bom dia!");
 
             Console.ReadLine();
         }
